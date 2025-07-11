8fae435792b97e1530e7570526b4918b607dc4d5
ChainSpider (ChainSpider) <getShelukhin@gmail.com>
2025-07-11 20:47:24 +0300

    web_response

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 from fastapi import FastAPI
+from fastapi.responses import HTML_Response
 
 app = FastAPI()
 
-@app.get("/")
-async def read_root():
-    return {"message": "Hello World"}
+@app.get("/", read_class=HTML_Response)
+async def read_alternative():
+    html_content = """
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <title>Альтернативный метод</title>
+        <head>
+        <body>
+            <h1>Some text</h1>
+            <p>Another scenario</p>
+        </body>
+    </html>
+    """
+    return html_content
