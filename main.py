9e41cf67375d4124b63bf47b0d5c819012831cc3
ChainSpider (ChainSpider) <getShelukhin@gmail.com>
2025-07-13 12:05:12 +0300

    StaticFiles

--- main.py
+++ main.py
@@ -1,23 +1,8 @@
 from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
-def generate_html_response():
-    html_content = """
-    <!DOCTYPE html>
-    <html>
-        <head>
-            <title>Альтернативный метод</title>
-        </head>
-        <body>
-            <h1>Some text</h1>
-            <p>Another scenario</p>
-        </body>
-    </html>
-    """
-    return HTMLResponse(content=html_content, status_code=200)
-
-@app.get("/items/", response_class=HTMLResponse)
+@app.get("/items/", response_class=StaticFiles)
 async def read_items():
-    return generate_html_response()
+    return app.mount("/", StaticFiles(directory="static", html = True), name="static")
